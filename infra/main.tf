5f7e5d3de0a74e469a0696f94840624c9ed37b59
danielmartincraig <danielmartincraig@gmail.com>
2025-01-03 16:37:51 -0600

    updated tf version

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
+
 
 resource "aws_eks_cluster" "example" {
   name     = "example-cluster"
