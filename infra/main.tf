ec32a9b1a4f9119db6c996c3be01e6322ee4dbb6
danielmartincraig <danielmartincraig@gmail.com>
2025-01-06 15:20:42 -0600

    Added nginx pods once again

--- infra/main.tf
+++ infra/main.tf
@@ -72,3 +72,61 @@ data "aws_availability_zones" "available" {}
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
+
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:1.14.2"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx"
+  }
+
+  spec {
+    selector = {
+      app = "nginx"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
