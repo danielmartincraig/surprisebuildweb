a509424a3640d14a31b6f75134b482c88b772d96
danielmartincraig <danielmartincraig@gmail.com>
2025-01-06 07:32:23 -0600

    shuffled availability zones of subnet pair

--- infra/main.tf
+++ infra/main.tf
@@ -57,9 +57,12 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSServicePolicy" {
 
 resource "aws_subnet" "example" {
   count = 2
-  vpc_id     = aws_vpc.example.id
-  cidr_block = cidrsubnet(aws_vpc.example.cidr_block, 8, count.index)
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = cidrsubnet(aws_vpc.example.cidr_block, 8, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
 }
+
+data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
