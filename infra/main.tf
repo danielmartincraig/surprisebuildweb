dfac4ef1ca107f538d812dcdf9c4a9db45c189b3
danielmartincraig <danielmartincraig@gmail.com>
2025-01-08 22:16:42 -0600

    added kubernetes provider authentication config

--- infra/main.tf
+++ infra/main.tf
@@ -23,6 +23,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "kubernetes" {
+  host                   = aws_eks_cluster.example.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.example.certificate_authority[0].data)
+  token                  = aws_eks_cluster_auth.example.token
+}
 
 resource "aws_eks_cluster" "example" {
   name     = "example-cluster"
