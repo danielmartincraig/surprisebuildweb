04fada68502be125c1711c2219272d11f32f67f4
danielmartincraig <danielmartincraig@gmail.com>
2025-01-24 21:44:23 -0600

    Changed main.tf to use ecs instead of eks

--- infra/main.tf
+++ infra/main.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.6.1"
-    }
   }
   cloud { 
     organization = "surprisebuild"
@@ -23,54 +18,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_eks_cluster" "example" {
-  name = aws_eks_cluster.example.name
-}
-
-data "aws_eks_cluster_auth" "example" {
-  name = aws_eks_cluster.example.name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.example.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.example.token
-}
-
-resource "aws_eks_cluster" "example" {
-  name     = "example-cluster"
-  role_arn = aws_iam_role.cluster.arn
-
-  vpc_config {
-    subnet_ids = aws_subnet.example[*].id
-  }
-}
-
-resource "aws_iam_role" "cluster" {
-  name = "eks-cluster-example-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cluster.name
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.cluster.name
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "example" {
@@ -82,100 +31,62 @@ resource "aws_subnet" "example" {
 
 data "aws_availability_zones" "available" {}
 
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_ecs_cluster" "example" {
+  name = "example-cluster"
 }
 
-resource "kubernetes_deployment" "example" {
-  metadata {
-    name = "terraform-example"
-    labels = {
-      test = "MyExampleApp"
-    }
-  }
+resource "aws_ecs_task_definition" "example" {
+  family                   = "example-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
 
-  spec {
-    replicas = 3
-
-    selector {
-      match_labels = {
-        test = "MyExampleApp"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          test = "MyExampleApp"
+  container_definitions = jsonencode([
+    {
+      name      = "example"
+      image     = "nginx:1.21.6"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
         }
-      }
-
-      spec {
-        container {
-          image = "nginx:1.21.6"
-          name  = "example"
-
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-
-          liveness_probe {
-            http_get {
-              path = "/"
-              port = 80
-
-              http_header {
-                name  = "X-Custom-Header"
-                value = "Awesome"
-              }
-            }
-
-            initial_delay_seconds = 3
-            period_seconds        = 3
-          }
-        }
-      }
+      ]
     }
+  ])
+}
+
+resource "aws_ecs_service" "example" {
+  name            = "example-service"
+  cluster         = aws_ecs_cluster.example.id
+  task_definition = aws_ecs_task_definition.example.arn
+  desired_count   = 3
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = aws_subnet.example[*].id
+    security_groups  = [aws_security_group.example.id]
+    assign_public_ip = true
   }
 }
 
-resource "kubernetes_service" "example" {
-  metadata {
-    name = "terraform-example"
-  }
-  spec {
-    selector = {
-      app = kubernetes_pod.example.metadata.0.labels.app
-    }
-    session_affinity = "ClientIP"
-    port {
-      port        = 8080
-      target_port = 80
-    }
+resource "aws_security_group" "example" {
+  name        = "example-sg"
+  description = "Allow HTTP traffic"
+  vpc_id      = aws_vpc.example.id
 
-    type = "LoadBalancer"
-  }
-}
-
-resource "kubernetes_pod" "example" {
-  metadata {
-    name = "terraform-example"
-    labels = {
-      app = "MyApp"
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  spec {
-    container {
-      image = "nginx:1.21.6"
-      name  = "example"
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
