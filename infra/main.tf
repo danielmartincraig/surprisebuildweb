427ab2bc28da0c686497d15b8ed0df05aa090f71
danielmartincraig <danielmartincraig@gmail.com>
2025-01-31 09:29:27 -0600

    Added cloudwatch logging config

--- infra/main.tf
+++ infra/main.tf
@@ -50,6 +50,28 @@ resource "aws_subnet" "example" {
 
 data "aws_availability_zones" "available" {}
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_cluster" "example" {
   name = "example-cluster"
 }
@@ -60,6 +82,7 @@ resource "aws_ecs_task_definition" "example" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -72,6 +95,14 @@ resource "aws_ecs_task_definition" "example" {
           hostPort      = 80
         }
       ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/example"
+          awslogs-region        = "us-east-1"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
     }
   ])
 }
@@ -88,6 +119,11 @@ resource "aws_ecs_service" "example" {
     security_groups  = [aws_security_group.example.id]
     assign_public_ip = true
   }
+}
+
+resource "aws_logs_log_group" "example" {
+  name              = "/ecs/example"
+  retention_in_days = 1
 }
 
 resource "aws_security_group" "example" {
