c7838c1a228871965d80ca325b95e875fd610458
danielmartincraig <danielmartincraig@gmail.com>
2025-02-08 17:24:52 -0600

    corrected lb target group type

--- infra/main.tf
+++ infra/main.tf
@@ -165,7 +165,7 @@ resource "aws_lb_target_group" "example" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.example.id
-  target_type = "alb"
+  target_type = "ip"
 
   health_check {
     path                = "/"
