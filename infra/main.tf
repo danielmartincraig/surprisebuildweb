bbe994c67df417db61bb9aba01147fcdd3ac8076
danielmartincraig <danielmartincraig@gmail.com>
2025-02-19 00:08:31 -0600

    corrected error in acm name

--- infra/main.tf
+++ infra/main.tf
@@ -267,7 +267,7 @@ resource "aws_route53_record" "auth-cognito-A" {
 
 resource "aws_route53_record" "dns-validation" {
   for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.parent_acm.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
