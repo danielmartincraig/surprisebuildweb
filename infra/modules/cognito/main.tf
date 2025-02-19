5443a13be8efd0b9fb799a9b4d3261072e284f49
danielmartincraig <danielmartincraig@gmail.com>
2025-02-18 22:08:16 -0600

    changed client_id to id in cognito tf

--- infra/modules/cognito/main.tf
+++ infra/modules/cognito/main.tf
@@ -1,15 +1,3 @@
-resource "aws_cognito_identity_pool" "example" {
-  identity_pool_name               = "example_identity_pool"
-  allow_unauthenticated_identities = false
-  allow_classic_flow               = false
-
-  cognito_identity_providers {
-    client_id               = aws_cognito_user_pool_client.example.client_id
-    provider_name           = aws_cognito_user_pool.example.endpoint
-    server_side_token_check = true
-  }
-}
-
 resource "aws_cognito_user_pool" "example" {
   name = "example_user_pool"
 }
@@ -18,3 +6,16 @@ resource "aws_cognito_user_pool_client" "example" {
   name         = "example_user_pool_client"
   user_pool_id = aws_cognito_user_pool.example.id
 }
+
+resource "aws_cognito_identity_pool" "example" {
+  identity_pool_name               = "example_identity_pool"
+  allow_unauthenticated_identities = false
+  allow_classic_flow               = false
+
+  cognito_identity_providers {
+    client_id               = aws_cognito_user_pool_client.example.id
+    provider_name           = aws_cognito_user_pool.example.endpoint
+    server_side_token_check = true
+  }
+}
+
