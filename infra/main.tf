5a676980091f5c5ec6731972802a23c547748740
danielmartincraig <danielmartincraig@gmail.com>
2025-02-19 00:04:47 -0600

    Added dns validation record

--- infra/main.tf
+++ infra/main.tf
@@ -263,6 +263,23 @@ resource "aws_route53_record" "auth-cognito-A" {
   }
 }
 
+resource "aws_route53_record" "dns-validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.example.zone_id
+}
+
 resource "aws_acm_certificate_validation" "example" {
   certificate_arn         = aws_acm_certificate.parent_acm.arn
   validation_record_fqdns = [for record in aws_route53_record.example_cert_validation : record.fqdn]
