f36fa7c1acdda2626f299afc0e98b82042def9f8
danielmartincraig <danielmartincraig@gmail.com>
2025-03-22 22:12:35 -0500

    added allows_oath_flows and allowed_oauth_scopes

--- infra/main.tf
+++ infra/main.tf
@@ -295,7 +295,9 @@ resource "aws_cognito_user_pool" "example" {
 resource "aws_cognito_user_pool_client" "example" {
   name         = "example_user_pool_client"
   user_pool_id = aws_cognito_user_pool.example.id
-
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code", "implicit"]
+  allowed_oauth_scopes                 = ["email", "openid"]
   callback_urls = ["http://localhost:8080/", "https://www.surprisebuild.com/"]
 
   explicit_auth_flows = [
