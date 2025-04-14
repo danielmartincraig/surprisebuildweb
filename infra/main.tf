2b3ae80c2e4c4189fa402ad20c3ffe9768f95696
danielmartincraig <danielmartincraig@gmail.com>
2025-04-14 10:20:19 -0500

    added cloudfront distribution

--- infra/main.tf
+++ infra/main.tf
@@ -336,3 +336,78 @@ resource "aws_cognito_user_pool_domain" "example" {
 resource "aws_s3_bucket" "example" {
   bucket = "surprisebuild-resources-bucket"
 }
+
+resource "aws_cloudfront_origin_access_control" "example" {
+  name          = "surprisebuild-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "example" {
+  origin {
+    domain_name = aws_s3_bucket.example.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.example.id}"
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.example.id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+
+  default_cache_behavior {
+    target_origin_id       = local.s3_origin_id
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US"]
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.parent_acm.arn
+    ssl_support_method        = "sni-only"
+    minimum_protocol_version  = "TLSv1.2_2021"
+  }
+}
+
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontAccess"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.example.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.example.arn
+          }
+        }
+      }
+    ]
+  })
+}
